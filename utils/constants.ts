6af8a6747a75468fa6f57a6bf5da6369e409ab3b
hammad1029 <hammad1029@gmail.com>
2025-02-28 04:19:31 +0500

    seeder

--- utils/constants.ts
+++ utils/constants.ts
@@ -6,8 +6,8 @@ export const passport = {
 export const env = {
   jwtSecret: process.env.JWT_SECRET || "oooo",
   sessionSecret: process.env.SESSION_SECRET || "oooo",
-  port: process.env.PORT || 3000
+  port: process.env.PORT || 3001
 };
 
-export const defaultPassword = "hello123"
+export const defaultPassword = process.env.DEFAULT_PASSWORD || "hello123"
 export const bcryptRounds = 10
